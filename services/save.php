cd40ed8af4fd4b4362b2cddd474efc7f55f6fc4d
scam627 <stiven.cardona627@gmail.com>
2019-09-11 23:51:15 -0500

    admin video function first version

---
+++ services/save.php
@@ -0,0 +1,10 @@
+<?php
+    $conn = mysqli_connect("localhost", "root", "1234", "freelancer");
+    $title = $_POST['title'];
+    $token = $_POST['token'];
+    $description = $_POST['description'];
+    $result = $conn->query("INSERT INTO videos (token, description, title) VALUES ('$token','$description','$title')");
+    if($result){
+        header("Location: ../manage.html");
+    };
+?>
