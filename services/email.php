fd132c8e94f18210f03e177796f9052d39b2c0e6
scam627 <stiven.cardona627@gmail.com>
2019-10-22 12:23:15 -0500

    email update

--- services/email.php
+++ services/email.php
@@ -1,7 +1,7 @@
 <?php
 	$mensaje = "Contacto: ".$_POST["email"]."\nMensaje: ".$_POST["mensaje"]."\n";
 
-	if(mail("stiven.cardona627@gmail.com", "Contacto", $mensaje))
+	if(mail("andresjaramilloalvarez@hotmail.com", "Contacto", $mensaje))
 		header('Location: ../index.php?status=ok');
 	else
 		header('Location: ../error.php');
