d8dfc87503e39ce0f1190e2b9d76f00e52539fcb
scam627 <stiven.cardona627@gmail.com>
2019-10-22 11:31:24 -0500

    enable email service

--- services/email.php
+++ services/email.php
@@ -1,5 +1,5 @@
 <?php
-	$mensaje = "Contacto: ".$_POST["email"]."\n Mensaje: ".$_POST["mensaje"]."\n";
+	$mensaje = "Contacto: ".$_POST["email"]."\nMensaje: ".$_POST["mensaje"]."\n";
 
 	if(mail("stiven.cardona627@gmail.com", "Contacto", $mensaje))
 		echo "Mensaje enviado";
