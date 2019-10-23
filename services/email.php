6d9cff3ce908eb6e0a50a4337aa94255ad3f32e4
scam627 <stiven.cardona627@gmail.com>
2019-10-22 20:55:21 -0500

    service email enable

--- services/email.php
+++ services/email.php
@@ -4,12 +4,12 @@
 	$message = isset($_POST["mensaje"]) ? $_POST["mensaje"] : " ";
 	$telefono = isset($_POST["telefono"]) ? $_POST["telefono"] : " ";
 	$mensaje = "Contacto: ".$email."\nNombre:".$name."\nMensaje: ".$message."\nTeléfono:".$telefono."\n";
-	if(mail("stiven.cardona627@gmail.com", "Contacto", $mensaje)){
+	if(mail("andresjaramilloalvarez@hotmail.com", "Contacto", $mensaje)){
 		if(isset($_POST["email"])){
 			$correo = "Hola $name\nMuchas gracias por escribirnos.\nNos encanta saber que estas comprometido con tener una vida más feliz y productiva.\n\nSoy Andrés Jaramillo una persona como Tu, llena de sueños, con una linda familia conformada de tres Hijos, Santiago y los Mellizos Alicia y Juan Juan, una hermosa esposa Catalina y toda una vida llena de milagros, Tipping points, historias y aprendizajes por lo que le agradezco cada segundo a Dios.\n\nPronto me comunicare contigo, para que juntos descubramos tus milagros y agradezcamos por ellos.\n\nSer Feliz y Productivo solo depende de Ti y lo mejor de todo es Gratis.\n\nUn abrazo\n\nJara";
-			var_dump(mail($email,"Andres Mauricio Jaramillo Alvarez",$correo));
-			header('Location: ../index.php?status=ok');
+			mail($email,"Andres Mauricio Jaramillo Alvarez",$correo);
 		}
+		header('Location: ../index.php?status=ok');
 	}
 	else
 		header('Location: ../index.php?status=fail');
